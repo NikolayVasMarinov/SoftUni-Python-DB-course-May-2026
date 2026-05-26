b87862cb325cbc6adf4cc4d684018f937887443f
Nikolay Marinov <nik.marinov09@gmail.com>
2026-05-26 17:58:16 +0300

    Queries for data types and table basics exercise lecture, queries 11-12

--- Data_Types_and_Table_Basics_Exercise/queries.sql
+++ Data_Types_and_Table_Basics_Exercise/queries.sql
@@ -45,3 +45,16 @@ ALTER COLUMN task TYPE VARCHAR(150);
 ALTER TABLE minions_info
 ALTER COLUMN equipped
 DROP NOT NULL;
+
+ALTER TABLE minions_info
+DROP age;
+
+CREATE TABLE minions_birthdays(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50),
+    date_of_birth DATE,
+    age INT,
+    present VARCHAR(100),
+    party TIMESTAMPTZ
+)
+
