a51a8d8912cf077d906e9e70c635a09c05997dfb
Nikolay Marinov <nik.marinov09@gmail.com>
2026-05-26 17:50:03 +0300

    Queries for data types and table basics exercise lecture, queries 6-10

--- Data_Types_and_Table_Basics_Exercise/queries.sql
+++ Data_Types_and_Table_Basics_Exercise/queries.sql
@@ -21,3 +21,27 @@ ALTER TABLE minions_info
 ADD COLUMN email VARCHAR(20),
 ADD COLUMN equipped BOOLEAN NOT NULL;
 
+CREATE TYPE type_mood AS ENUM(
+    'happy',
+    'relaxed',
+    'stressed',
+    'sad'
+);
+ALTER TABLE minions_info
+ADD COLUMN mood type_mood;
+
+ALTER TABLE minions_info
+ALTER COLUMN age SET DEFAULT(0),
+ALTER COLUMN name SET DEFAULT(''),
+ALTER COLUMN code SET DEFAULT('');
+
+ALTER TABLE minions_info
+ADD CONSTRAINT unique_constraint UNIQUE (id, email),
+ADD CONSTRAINT banana_check CHECK (banana > 0);
+
+ALTER TABLE minions_info
+ALTER COLUMN task TYPE VARCHAR(150);
+
+ALTER TABLE minions_info
+ALTER COLUMN equipped
+DROP NOT NULL;
