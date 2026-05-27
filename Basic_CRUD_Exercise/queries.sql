812dfbb87ebc63baa0ea6de51f4c427a1584582d
Nikolay Marinov <nik.marinov09@gmail.com>
2026-05-27 18:36:51 +0300

    Queries for basic CRUD exercise lecture, queries 9-15

--- Basic_CRUD_Exercise/queries.sql
+++ Basic_CRUD_Exercise/queries.sql
@@ -65,18 +65,64 @@ WHERE start_date >= '2016-06-01 07:00:00' AND end_date < '2023-06-04 00:00:00'
 ORDER BY start_date;
 
 --QUERY 9
+SELECT
+    number,
+    street
+FROM addresses
+WHERE id BETWEEN 50 AND 100 OR number < 1000;
 
 --QUERY 10
+SELECT
+    employee_id,
+    project_id
+FROM employees_projects
+WHERE employee_id IN (200, 250) AND project_id NOT IN (50, 100);
 
 --QUERY 11
+SELECT
+    name,
+    start_date
+FROM projects
+WHERE name IN ('Mountain', 'Road', 'Touring') LIMIT 20;
 
 --QUERY 12
+SELECT
+    first_name || ' ' || last_name as "full_name",
+    job_title,
+    salary
+FROM employees
+WHERE salary IN (12500, 14000, 23600, 25000) LIMIT 20
+ORDER BY salary DESC;
 
 --QUERY 13
+SELECT
+    id,
+    first_name,
+    last_name
+FROM employees
+WHERE middle_name IS NULL
+LIMIT 3;
 
 --QUERY 14
+INSERT INTO
+    departments(department, manager_id)
+VALUES
+    ('Finance', 3),
+	('Information Services', 42),
+	('Document Control', 90),
+	('Quality Assurance', 274),
+	('Facilities and Maintenance', 218),
+	('Shipping and Receiving', 85),
+	('Executive', 109);
 
 --QUERY 15
+CREATE TABLE company_chart
+AS SELECT
+       first_name || ' ' || last_name as "full_name",
+       job_title,
+       department_id,
+       manager_id
+FROM employees;
 
 --QUERY 16
 
