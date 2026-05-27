571da938790da6389c20e283fcd1fc563fd013f6
Nikolay Marinov <nik.marinov09@gmail.com>
2026-05-27 17:03:29 +0300

    Queries for basic CRUD lecture, queries 3-5

--- Basic_CRUD/queries.sql
+++ Basic_CRUD/queries.sql
@@ -1,9 +1,11 @@
+-- QUERY 1
 SELECT
     id,
     first_name || ' ' || last_name AS "Full Name",
     job_title AS "Job Title"
 FROM employees;
 
+-- QUERY 2
 SELECT
     id,
     first_name || ' ' || last_name AS full_name,
@@ -12,3 +14,29 @@ SELECT
 FROM employees
 WHERE salary > 1000
 ORDER BY id;
+
+-- QUERY 3
+SELECT
+    *
+FROM employees
+WHERE salary >= 1000 AND department_id = 4
+ORDER BY id;
+
+-- QUERY 4
+INSERT INTO
+    employees(first_name, last_name, job_title, department_id, salary)
+VALUES
+    ('Samantha', 'Young', 'Housekeeping', '4', '900'),
+    ('Roger', 'Palmer', 'Waiter', '3', '928.33');
+
+SELECT * from employees;
+
+--QUERY 5
+UPDATE
+    employees
+SET
+    salary = salary + 100
+WHERE job_title = 'Manager';
+
+SELECT * from employees
+WHERE job_title = 'Manager';
