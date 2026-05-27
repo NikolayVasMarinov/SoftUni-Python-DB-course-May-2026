d96210fac211c8db6b66a2ed64ef89aef4e3c3cd
Nikolay Marinov <nik.marinov09@gmail.com>
2026-05-27 18:22:41 +0300

    Queries for basic CRUD exercise lecture, queries 6-8

--- Basic_CRUD_Exercise/queries.sql
+++ Basic_CRUD_Exercise/queries.sql
@@ -32,14 +32,69 @@ FROM employees
 ORDER BY first_name LIMIT 50;
 
 --QUERY 5
-SELECT id,
-       first_name || ' ' || middle_name || ' ' || last_name as "full_name",
-       hire_date
+SELECT
+    id,
+    first_name || ' ' || middle_name || ' ' || last_name as "full_name",
+    hire_date
 FROM employees
-ORDER BY hire_date
-OFFSET 9;
+ORDER BY hire_date OFFSET 9;
 
 --QUERY 6
-
+SELECT
+    id,
+    number || ' ' || street as "address",
+    city_id
+FROM addresses
+WHERE id >= 20;
 
 --QUERY 7
+SELECT
+    number || ' ' || street as "address",
+    city_id
+FROM addresses
+WHERE city_id >= 0 AND city_id % 2 = 0
+ORDER BY city_id;
+
+--QUERY 8
+SELECT
+    name,
+    start_date,
+    end_date
+FROM projects
+WHERE start_date >= '2016-06-01 07:00:00' AND end_date < '2023-06-04 00:00:00'
+ORDER BY start_date;
+
+--QUERY 9
+
+--QUERY 10
+
+--QUERY 11
+
+--QUERY 12
+
+--QUERY 13
+
+--QUERY 14
+
+--QUERY 15
+
+--QUERY 16
+
+--QUERY 17
+
+--QUERY 18
+
+--QUERY 19
+
+--QUERY 20
+
+--QUERY 21
+
+--QUERY 22
+
+--QUERY 23
+
+--QUERY 24
+
+--QUERY 25
+
