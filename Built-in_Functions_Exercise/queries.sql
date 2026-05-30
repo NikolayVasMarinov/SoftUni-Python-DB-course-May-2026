b4844d0e39e1d7026b62d3d2338d5f585767ca4d
Nikolay Marinov <nik.marinov09@gmail.com>
2026-05-30 08:59:41 +0300

    Queries for Built-in Functions exercise lecture, queries 1-6

---
+++ Built-in_Functions_Exercise/queries.sql
@@ -0,0 +1,107 @@
+/*
+
+*/
+-- QUERY 1
+CREATE VIEW view_river_info
+AS SELECT
+       CONCAT_WS(' ', 'The river', river_name,
+              'flows into the', outflow,
+              'and is', length, 'kilometers long.') AS "River Information"
+FROM rivers
+ORDER BY river_name;
+
+-- QUERY 2
+CREATE VIEW view_continents_countries_currencies_details
+AS
+SELECT CONCAT(cont.continent_name, ': ', cont.continent_code)                             AS continent_details,
+       CONCAT_WS(' - ', countr.country_name, countr.capital, countr.area_in_sq_km, 'km2') AS country_information,
+       CONCAT(curr.description, ' (', curr.currency_code, ')')                            AS currencies
+FROM continents AS cont
+         JOIN countries AS countr ON cont.continent_code = countr.continent_code
+         JOIN currencies AS curr ON countr.currency_code = curr.currency_code
+ORDER BY country_information, currencies;
+
+-- QUERY 3
+ALTER TABLE countries
+ADD COLUMN capital_code CHAR(2);
+
+UPDATE countries
+SET capital_code = SUBSTRING(capital, 1, 2);
+
+-- QUERY 4
+SELECT
+    SUBSTRING(description, 5)
+FROM currencies;
+
+--QUERY 5
+SELECT
+    UNNEST(REGEXP_MATCHES("River Information", '([0-9]{1,4})')) AS river_length
+FROM view_river_info;
+
+--QUERY 6
+SELECT
+    REPLACE(mountain_range, 'a', '@') AS replace_a,
+    REPLACE(mountain_range, 'A', '$') AS replace_A
+FROM mountains;
+
+--QUERY 7
+
+
+--QUERY 8
+
+
+--QUERY 9
+
+
+--QUERY 10
+
+
+--QUERY 11
+
+
+--QUERY 12
+
+
+--QUERY 13
+
+
+--QUERY 14
+
+
+--QUERY 15
+
+
+--QUERY 16
+
+
+--QUERY 17
+
+
+--QUERY 18
+
+
+--QUERY 19
+
+
+--QUERY 20
+
+
+--QUERY 21
+
+
+--QUERY 22
+
+
+--QUERY 23
+
+
+--QUERY 24
+
+
+--QUERY 25
+
+
+--QUERY 26
+
+
+--QUERY 27
