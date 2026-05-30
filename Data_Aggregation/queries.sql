1d865d50d0b15bc0c3c6146df3c22274b3d174ac
Nikolay Marinov <nik.marinov09@gmail.com>
2026-05-30 21:30:40 +0300

    Queries for Data Aggregation lecture, queries 3-7

--- Data_Aggregation/queries.sql
+++ Data_Aggregation/queries.sql
@@ -18,15 +18,44 @@ GROUP BY department_id
 ORDER BY department_id;
 
 -- QUERY 3
-
+SELECT
+    department_id,
+    SUM(salary) AS total_salaries
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
 
 --QUERY 4
-
+SELECT
+    department_id,
+    MAX(salary) AS total_salaries
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
 
 --QUERY 5
-
+SELECT
+    department_id,
+    MIN(salary) AS total_salaries
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
 
 --QUERY 6
-
+SELECT
+    department_id,
+    AVG(salary) AS total_salaries
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
 
 --QUERY 7
+SELECT
+    department_id,
+    SUM(salary) AS "Total Salary"
+FROM employees
+GROUP BY department_id
+HAVING "Total Salary" < 4200
+ORDER BY department_id;
+
+--QUERY 8
