6708f54b2b0d70ff5aa1895d1fba94c01d3fbd28
Nikolay Marinov <nik.marinov09@gmail.com>
2026-05-31 13:36:45 +0300

    Queries for Data Aggregation Exercise lecture, queries 1-5

---
+++ Data_Aggregation_Exercise/queries.sql
@@ -0,0 +1,72 @@
+/*
+    Description:
+*/
+--QUERY 1
+SELECT
+    COUNT(id)
+FROM wizard_deposits;
+
+--QUERY 2
+SELECT
+    SUM(deposit_amount)
+FROM wizard_deposits;
+
+--QUERY 3
+SELECT
+    ROUND(AVG(magic_wand_size), 3)
+FROM wizard_deposits;
+
+--QUERY 4
+SELECT
+    MIN(deposit_charge)
+FROM wizard_deposits;
+
+--QUERY 5
+SELECT
+    MAX(age)
+FROM wizard_deposits;
+
+--QUERY 6
+
+
+--QUERY 7
+
+
+--QUERY 8
+
+
+--QUERY 9
+
+
+--QUERY 10
+
+
+--QUERY 11
+
+
+--QUERY 12
+
+
+--QUERY 13
+
+
+--QUERY 14
+
+
+--QUERY 15
+
+
+--QUERY 16
+
+
+--QUERY 17
+
+
+--QUERY 18
+
+
+--QUERY 19
+
+
+--QUERY 20
+
