d6e88e68287ca75fc82df3cf254d1886edf55015
Nikolay Marinov <nik.marinov09@gmail.com>
2026-06-07 16:00:12 +0300

    Queries for Table relations lecture, queries 4-5

--- Table_Relations/queries.sql
+++ Table_Relations/queries.sql
@@ -37,6 +37,54 @@ FROM
     JOIN routes AS r ON c.id = r.leader_id;
 
 --QUERY 4
+DROP TABLE mountains;
+DROP TABLE peaks;
 
+CREATE TABLE mountains (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE peaks (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50),
+    mountain_id INT,
+
+    CONSTRAINT fk_mountain_id
+        FOREIGN KEY (mountain_id)
+        REFERENCES mountains(id) ON DELETE CASCADE
+);
 
 --QUERY 5
+CREATE DATABASE project_management;
+
+CREATE TABLE clients (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE projects (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    client_id INT,
+    project_lead_id INT,
+
+    CONSTRAINT fk_client_id
+        FOREIGN KEY (client_id)
+            REFERENCES clients(id)
+);
+
+CREATE TABLE employees (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    project_id INT,
+
+    CONSTRAINT fk_project_id
+        FOREIGN KEY (project_id)
+            REFERENCES projects(id)
+);
+
+ALTER TABLE projects
+ADD CONSTRAINT fk_project_lead_id
+        FOREIGN KEY (project_lead_id)
+            REFERENCES employees(id);
