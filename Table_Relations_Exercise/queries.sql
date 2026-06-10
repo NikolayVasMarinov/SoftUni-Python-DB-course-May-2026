17977b3ed7524550b8ee7c53e32b3b3bcac116cb
Nikolay Marinov <nik.marinov09@gmail.com>
2026-06-10 19:31:10 +0300

    Queries for Table relations exercise lecture, query 8

--- Table_Relations_Exercise/queries.sql
+++ Table_Relations_Exercise/queries.sql
@@ -116,7 +116,57 @@ VALUES
     ('philip_1123.com', 'Roman Theater', 2);
 
 --QUERY 8
+CREATE DATABASE study_session_db;
 
+CREATE TABLE students (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    student_name VARCHAR(20)
+);
+
+CREATE TABLE exams (
+    id INT GENERATED ALWAYS AS IDENTITY (START WITH 101 INCREMENT BY 1) PRIMARY KEY,
+    exam_name VARCHAR(30)
+);
+
+CREATE TABLE study_halls (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    study_hall_name VARCHAR(30),
+    exam_id INT REFERENCES exams(id)
+);
+
+CREATE TABLE students_exams (
+    student_id INT REFERENCES students(id),
+    exam_id INT REFERENCES exams(id),
+
+    CONSTRAINT pk_students_exams
+        PRIMARY KEY (student_id, exam_id)
+);
+
+INSERT INTO students(student_name)
+VALUES
+    ('Mila'),
+    ('Toni'),
+    ('Ron');
+
+INSERT INTO exams(exam_name)
+VALUES
+    ('Python Advanced'),
+    ('Python OOP'),
+    ('PostgreSQL');
+
+INSERT INTO study_halls(study_hall_name, exam_id)
+VALUES
+    ('Open Source Hall', 102),
+    ('Inspiration Hall', 101),
+    ('Creative Hall', 103),
+    ('Masterclass Hall', 103),
+    ('Information Security Hall', 103);
+
+INSERT INTO students_exams(student_id, exam_id)
+VALUES
+    (1, 101),
+    (1, 102),
+    (2, 101);
 
 --QUERY 9
 
