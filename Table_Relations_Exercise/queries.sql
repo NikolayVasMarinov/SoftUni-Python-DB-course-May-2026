3a0645b4cbe1b1c2b17d1f384537c4816ae783ed
Nikolay Marinov <nik.marinov09@gmail.com>
2026-06-10 19:04:07 +0300

    Queries for Table relations exercise lecture, query 4

--- Table_Relations_Exercise/queries.sql
+++ Table_Relations_Exercise/queries.sql
@@ -45,7 +45,45 @@ VALUES
     ('Yana', 60200.0000, 100);
 
 --QUERY 4
+CREATE DATABASE car_manufacture_db;
 
+CREATE TABLE manufacturers (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(15),
+);
+
+CREATE TABLE models (
+    id INT GENERATED ALWAYS AS IDENTITY (START WITH 1000 INCREMENT BY 1) PRIMARY KEY,
+    model_name VARCHAR(20),
+    manufacturer_id INT REFERENCES manufacturers(id)
+);
+
+CREATE TABLE production_years (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    established_on DATE,
+    manufacturer_id INT REFERENCES manufacturers(id)
+);
+
+INSERT INTO manufacturers(name)
+VALUES
+    ('BMW'),
+    ('Tesla'),
+    ('Lada');
+
+INSERT INTO models(model_name, manufacturer_id)
+VALUES
+    ('X1', 1),
+    ('i6', 1),
+    ('Model S', 2),
+    ('Model X', 2),
+    ('Model 3', 2),
+    ('Nova', 3);
+
+INSERT INTO production_years(established_on, manufacturer_id)
+VALUES
+    ('1916-03-01', 1),
+    ('2003-01-01', 2),
+    ('1966-05-01', 3);
 
 --QUERY 5
 
