03f56f11c39b03f18890e80c06e8129f40f23a8f
Nikolay Marinov <nik.marinov09@gmail.com>
2026-06-10 19:14:23 +0300

    Queries for Table relations exercise lecture, query 6

--- Table_Relations_Exercise/queries.sql
+++ Table_Relations_Exercise/queries.sql
@@ -85,14 +85,35 @@ VALUES
     ('2003-01-01', 2),
     ('1966-05-01', 3);
 
---QUERY 5
-
-
 --QUERY 6
+CREATE DATABASE photo_shooting_db;
 
+CREATE TABLE customers (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(15),
+    date DATE
+);
 
---QUERY 7
+CREATE TABLE photos (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    url TEXT,
+    place VARCHAR(100),
+    customer_id INT REFERENCES customers(id)
+);
 
+INSERT INTO customers(name, date)
+VALUES
+    ('Bella', '2022-03-25'),
+    ('Philip', '2022-07-05');
+
+INSERT INTO photos(url, place, customer_id)
+VALUES
+    ('bella_1111.com', 'National Theatre', 1),
+    ('bella_1112.com', 'Largo', 1),
+    ('bella_1113.com', 'The View Restaurant', 1),
+    ('philip_1121.com', 'Old Town', 2),
+    ('philip_1122.com', 'Rowing Canal', 2),
+    ('philip_1123.com', 'Roman Theater', 2);
 
 --QUERY 8
 
