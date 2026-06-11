6eba75f4c2e530b1529cddc80577a9d6cba3db86
Nikolay Marinov <nik.marinov09@gmail.com>
2026-06-11 20:34:17 +0300

    Queries for Subqueries and JOINs lecture, queries 3-4

--- Subqueries_and_JOINs/queries.sql
+++ Subqueries_and_JOINs/queries.sql
@@ -25,6 +25,22 @@ ORDER BY e.employee_id
 LIMIT 5;
 
 --QUERY 3
-
+SELECT
+    e.employee_id,
+    CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+    p.project_id,
+    p.name AS project_name
+FROM employees_projects AS ep
+    JOIN employees AS e ON e.employee_id = ep.employee_id
+    JOIN projects AS p ON p.project_id = ep.project_id
+WHERE p.project_id = 1;
 
 --QUERY 4
+SELECT
+    COUNT(*)
+FROM employees
+WHERE salary > (
+    SELECT
+        AVG(salary)
+    FROM employees
+    );
