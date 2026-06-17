1c36cb3f2ea3343dae88d39f16d8a2809a6a38bb
Nikolay Marinov <nik.marinov09@gmail.com>
2026-06-17 20:10:40 +0300

    Queries for Subqueries and JOINs exercise lecture, queries 16-20

--- Subqueries_and_JOINs_Exercise/queries.sql
+++ Subqueries_and_JOINs_Exercise/queries.sql
@@ -153,10 +153,128 @@ WHERE country_code NOT IN (
         mountains_countries
     );
 
---QUERY 18
+--QUERY 16
+CREATE TABLE monasteries (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    monastery_name VARCHAR(255),
+    country_code CHAR(2)
+);
 
+INSERT INTO monasteries(monastery_name, country_code)
+VALUES
+      ('Rila Monastery "St. Ivan of Rila"', 'BG'),
+      ('Bachkovo Monastery "Virgin Mary"', 'BG'),
+      ('Troyan Monastery "Holy Mother''s Assumption"', 'BG'),
+      ('Kopan Monastery', 'NP'),
+      ('Thrangu Tashi Yangtse Monastery', 'NP'),
+      ('Shechen Tennyi Dargyeling Monastery', 'NP'),
+      ('Benchen Monastery', 'NP'),
+      ('Southern Shaolin Monastery', 'CN'),
+      ('Dabei Monastery', 'CN'),
+      ('Wa Sau Toi', 'CN'),
+      ('Lhunshigyia Monastery', 'CN'),
+      ('Rakya Monastery', 'CN'),
+      ('Monasteries of Meteora', 'GR'),
+      ('The Holy Monastery of Stavronikita', 'GR'),
+      ('Taung Kalat Monastery', 'MM'),
+      ('Pa-Auk Forest Monastery', 'MM'),
+      ('Taktsang Palphug Monastery', 'BT'),
+      ('Sümela Monastery', 'TR');
+
+ALTER TABLE countries
+ADD COLUMN three_rivers BOOLEAN DEFAULT FALSE;
+
+UPDATE countries
+SET three_rivers = (
+                SELECT
+                    COUNT(*) >= 3
+                FROM countries_rivers AS cr
+				WHERE cr.country_code = countries.country_code
+                );
+
+SELECT
+    m.monastery_name,
+    c.country_name
+FROM monasteries AS m
+JOIN countries AS c USING (country_code)
+WHERE NOT c.three_rivers
+ORDER BY m.monastery_name;
+
+--QUERY 17
+UPDATE countries
+SET country_name = 'Burma'
+WHERE country_name = 'Myanmar';
+
+INSERT INTO monasteries(monastery_name, country_code)
+VALUES
+    ('Hanga Abbey', (SELECT country_code FROM countries WHERE country_name = 'Tanzania')),
+    ('Myin-Tin-Daik', (SELECT country_code FROM countries WHERE country_name = 'Myanmar'));
+
+SELECT
+    con.continent_name,
+    cou.country_name,
+    COUNT(m.id) AS monasteries_count
+FROM countries AS cou
+JOIN continents AS con USING (continent_code)
+LEFT JOIN monasteries AS m USING (country_code)
+WHERE NOT cou.three_rivers
+GROUP BY cou.country_name, con.continent_name
+ORDER BY monasteries_count DESC, cou.country_name;
+
+--QUERY 18
+SELECT
+    tablename,
+    indexname,
+    indexdef
+FROM pg_indexes
+WHERE schemaname LIKE 'public'
+ORDER BY tablename, indexname;
 
 --QUERY 19
-
+CREATE VIEW "continent_currency_usage"
+AS
+SELECT
+    ra.continent_code,
+    ra.currency_code,
+    ra.currency_usage
+FROM (
+    SELECT
+        ct.continent_code,
+        ct.currency_code,
+        ct.currency_usage,
+        DENSE_RANK() OVER (PARTITION BY ct.continent_code ORDER BY ct.currency_usage DESC) AS ranking
+    FROM (
+        SELECT
+            continent_code,
+            currency_code,
+            COUNT(currency_code) AS currency_usage
+        FROM countries
+        GROUP BY continent_code, currency_code
+        HAVING COUNT(currency_code) > 1
+        ) AS ct
+    ) AS ra
+WHERE ranking = 1
+ORDER BY currency_usage DESC;
 
 --QUERY 20
+WITH "row_number" AS (
+    SELECT
+        c.country_name,
+		COALESCE(p.peak_name, '(no highest peak)') AS highest_peak_name,
+        COALESCE(p.elevation, 0) AS highest_peak_elevation,
+        COALESCE(m.mountain_range, '(no mountain)') AS mountain,
+        ROW_NUMBER() OVER(PARTITION BY country_name ORDER BY p.elevation DESC) AS row_num
+    FROM
+        countries AS c
+        LEFT JOIN mountains_countries AS mc USING (country_code)
+        LEFT JOIN mountains AS m ON m.id = mc.mountain_id
+        LEFT JOIN peaks AS p USING (mountain_id)
+)
+SELECT
+    country_name,
+    highest_peak_name,
+    highest_peak_elevation,
+    mountain
+FROM "row_number"
+WHERE row_num = 1
+ORDER BY country_name, highest_peak_elevation DESC;
